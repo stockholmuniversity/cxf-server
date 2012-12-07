fc423dcc3cfd8d0b0227407e29d7ed6e0e3ce15e
jqvar <jan.qvarnstrom@su.se>
2012-12-07 15:10:29 +0100

    Fix bougus auth return value

--- src/main/java/se/su/it/svc/SuCxfAuthenticator.java
+++ src/main/java/se/su/it/svc/SuCxfAuthenticator.java
@@ -90,7 +90,14 @@ public class SuCxfAuthenticator extends SpnegoAuthenticator {
       }
     }
 
-    return Authentication.UNAUTHENTICATED;
+    try{
+      ((HttpServletResponse) response).setHeader(HttpHeaders.WWW_AUTHENTICATE, "realm=\"" + _loginService.getName() + '"');
+      ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED);
+      return Authentication.SEND_CONTINUE;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return Authentication.SEND_CONTINUE;
+    }
   }
 
   protected boolean checkRole(String uid, String rURI) {
