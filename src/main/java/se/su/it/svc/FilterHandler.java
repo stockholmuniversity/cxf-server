b77a6f5d31e486aad766b23224cce3c5fca94973
Tommy Andersson <tommy.andersson@su.se>
2013-09-24 08:21:37 +0200

    DEVIDM-76: Some logging changes, fixes and improvements.

--- src/main/java/se/su/it/svc/FilterHandler.java
+++ src/main/java/se/su/it/svc/FilterHandler.java
@@ -33,26 +33,50 @@ package se.su.it.svc;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.eclipse.jetty.webapp.WebAppContext;
-import sun.rmi.runtime.Log;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+/**
+ *
+ */
 public class FilterHandler extends AbstractHandler {
+
+  /**
+   *
+   */
   private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(FilterHandler.class);
-  String tmpDir = null;
-  public FilterHandler(String tmpDir) {
-    this.tmpDir = tmpDir;
+
+  /**
+   *
+   */
+  private String tmpDir = null;
+
+  /**
+   *
+   * @param tmpDirx
+   */
+  public FilterHandler(final String tmpDirx) {
+    this.tmpDir = tmpDirx;
   }
-  public void handle(String target,Request baseRequest, HttpServletRequest request, HttpServletResponse response)
-    throws IOException, ServletException {
-    if(baseRequest.getRequestURI().equalsIgnoreCase("/status.html")) {
+
+  /**
+   *
+   * @param target
+   * @param baseRequest
+   * @param request
+   * @param response
+   * @throws IOException
+   * @throws ServletException
+   */
+  public final void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+
+    if (baseRequest.getRequestURI().equalsIgnoreCase("/status.html")) {
       response.setContentType("text/html");
       response.setStatus(HttpServletResponse.SC_OK);
       Properties versionProps = new Properties();
@@ -67,11 +91,13 @@ public class FilterHandler extends AbstractHandler {
         response.getWriter().println("Name: " + (name != null && name.length() > 0 ? name : noinfo) + "<br />");
         response.getWriter().println("Version: " + (version != null && version.length() > 0 ? version : noinfo) + "<br />");
         response.getWriter().println("Build Time: " + (builddate != null && builddate.length() > 0 ? builddate : noinfo) + "<br />");
-      } catch (Exception e){
-        logger.debug("Warning: Could not read version.properties file. " + e.getMessage());
+      } catch (Exception e) {
+        logger.debug("Warning: Could not read version.properties file. ", e);
         response.getWriter().println(noinfo);
       }
+
       response.getWriter().println("<br /><br />");
+
       try {
         FileInputStream in = new FileInputStream(tmpDir + "/webapp/version.properties");
         versionProps.load(in);
@@ -82,10 +108,11 @@ public class FilterHandler extends AbstractHandler {
         response.getWriter().println("Server Name: " + (name != null && name.length() > 0 ? name : noinfo) + "<br />");
         response.getWriter().println("Server Version: " + (version != null && version.length() > 0 ? version : noinfo) + "<br />");
         response.getWriter().println("Server Build Time: " + (builddate != null && builddate.length() > 0 ? builddate : noinfo) + "<br />");
-      } catch (Exception e){
-        logger.debug("Warning: Could not read server version.properties file. " + e.getMessage());
+      } catch (Exception e) {
+        logger.debug("Warning: Could not read server version.properties file. ", e);
         response.getWriter().println(noinfo);
       }
+
       baseRequest.setHandled(true);
       return;
     }
