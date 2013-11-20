00408ade1701f4cddd1ce9643bcd3a385feaf323
Joakim Lundin <joakim.lundin@su.se>
2013-11-20 10:01:12 +0100

    Correctly named variables.

--- src/main/java/se/su/it/svc/server/audit/AuditEntity.java
+++ src/main/java/se/su/it/svc/server/audit/AuditEntity.java
@@ -6,8 +6,8 @@ public class AuditEntity {
 
   private String created;
   private String operation;
-  private String text_args;
-  private String text_return;
+  private String textArgs;
+  private String textReturn;
   private String state;
   private List<String> methodDetails;
 
@@ -15,16 +15,16 @@ public class AuditEntity {
 
   public static AuditEntity getInstance(String created,
                                         String operation,
-                                        String text_args,
-                                        String text_return,
+                                        String textArgs,
+                                        String textReturn,
                                         String state,
                                         List<String> methodDetails) {
 
     AuditEntity auditEntity = new AuditEntity();
     auditEntity.created       = created;
     auditEntity.operation     = operation;
-    auditEntity.text_args     = text_args;
-    auditEntity.text_return   = text_return;
+    auditEntity.textArgs      = textArgs;
+    auditEntity.textReturn    = textReturn;
     auditEntity.state         = state;
     auditEntity.methodDetails = methodDetails;
     return auditEntity;
@@ -36,8 +36,8 @@ public class AuditEntity {
     sb.append(this.getClass().getName()).append("(");
     sb.append("created:").append(created).append(", ");
     sb.append("operation:").append(operation).append(", ");
-    sb.append("text_args:").append(text_args).append(", ");
-    sb.append("text_return:").append(text_return).append(", ");
+    sb.append("text_args:").append(textArgs).append(", ");
+    sb.append("text_return:").append(textReturn).append(", ");
     sb.append("state:").append(state).append(", ");
 
     sb.append("methodDetails:[");
